4a322b06958057e5f5e10b5d05af65c64d0bd6fe
LikeAshraful <maipalash35@gmail.com>
2024-02-24 14:57:08 +0600

    added some project releband image

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,44 +1,44 @@
 // Import images
 import WebImage1 from '../images/web-project-1.jpg';
-import WebImage2 from '../images/web-project-2.jpg';
+import Barta24Image from '../images/project-barta24.png';
 import MobileImage1 from '../images/mobile-project-1.jpg';
-import MobileImage2 from '../images/mobile-project-2.jpg';
-import UIImage1 from '../images/ui-project-1.jpg';
-import UIImage2 from '../images/ui-project-2.jpg';
+import GdsImage from '../images/gds.png';
+import EcommerceImage from '../images/ecommerce-project.png';
+import WebImage2 from '../images/web-project-2.jpg';
 
 export const projectsData = [
 	{
 		id: 1,
-		title: 'Barta24 - Multimedia News Portal Management System',
+		title: 'Barta24 - Multimedia News Portal',
 		category: 'Web Application',
-		img: WebImage2,
+		img: Barta24Image,
 		ProjectHeader: {
-			title: 'News Management UI - From Context',
-			publishDate: 'Jul 26, 2021',
-			tags: 'UI / Frontend',
+			title: 'Multimedia News Portal Management System',
+			publishDate: 'Jul, 2018',
+			tags: 'Backend / Frontend',
 		},
 	},
 	{
 		id: 2,
-		title: 'Campus/University Management System',
+		title: 'Online Flight Booking System (GDS)',
 		category: 'Web Application',
-		img: MobileImage2,
+		img: GdsImage,
 	},
 	{
 		id: 3,
-		title: 'E - Commerce Web App',
+		title: 'E - Commerce Web Application',
 		category: 'Web Application',
-		img: UIImage1,
+		img: EcommerceImage,
 	},
 	{
 		id: 4,
 		title: 'Human Resource Management System (HRMS)',
 		category: 'Web Application',
-		img: UIImage2,
+		img: WebImage2,
 	},
 	{
 		id: 5,
-		title: 'Accounting System',
+		title: 'Campus/University Management System',
 		category: 'Web Application',
 		img: MobileImage1,
 	},
